bf6a9d18f5f88ed4c71d87823b7c513bde6791c8
Rapkalin <raphael@myminicom.com>
2024-01-21 20:30:08 +0100

    01-homepage-structure - Init new footer

--- website/app/themes/bravada-child/content/landing-page.php
+++ website/app/themes/bravada-child/content/landing-page.php
@@ -8,11 +8,11 @@
 bravada_lpslider();
 // bravada_lpblocks();
 // bravada_lptext('one');
-bravada_lpboxes(1);
+// bravada_lpboxes(1);
 // bravada_lptext('two');
-bravada_lpboxes(2);
+// bravada_lpboxes(2);
 // bravada_lptext('three');
-bravada_lpindex();
+// bravada_lpindex();
 // bravada_lptext('four');
 		
 // FIN
--- website/app/themes/bravada-child/functions.php
+++ website/app/themes/bravada-child/functions.php
@@ -14,6 +14,8 @@ add_action( 'wp_enqueue_scripts', 'bravada_child_register_style', 11 );
 /* Load internationalisation / translations */
 add_action( 'after_setup_theme', 'bravada_child_theme_locale' );
 
+add_action( 'cryout_master_footerbottom_hook', 'bravada_child_footerbottom_hook', 9 );
+
 // Frontend side
 // require_once( get_stylesheet_directory() . "/includes/setup.php" );       	// Setup and init theme
 // require_once( get_stylesheet_directory() . "/includes/styles.php" );      	// Register and enqeue css styles and scripts
@@ -60,3 +62,29 @@ function bravada_child_theme_locale(): void
     load_child_theme_textdomain( 'bravada', get_stylesheet_directory() . '/languages' );
     load_child_theme_textdomain( 'bcrampon', get_stylesheet_directory() . '/languages' );
 }
+
+/**
+ * Remove the old footer and add the new one
+ * @return void
+ */
+function bravada_child_footerbottom_hook()
+{
+    # Remove the bravada's theme footer function to overide it
+    if (function_exists('bravada_master_footer')) {
+        remove_action( 'cryout_master_footerbottom_hook', 'bravada_master_footer' );
+        add_action('cryout_master_footerbottom_hook', 'bravada_child_master_footer', 11);
+    }
+}
+
+/**
+ * Output the new footer
+ * @return void
+ */
+function bravada_child_master_footer()
+{
+    do_action( 'cryout_footer_hook' );
+    echo '<div style="display:block; margin: 0.5em auto;">' . __( "Site by", "bravada" ) .
+        '<a target="_blank" href="' . "https://github.com/Rapkalin/" . '" title="';
+    echo 'Site by Freelance developer Rapkalin"> ' . 'Rapkalin</a> based on the Bravada Theme';
+}
+
